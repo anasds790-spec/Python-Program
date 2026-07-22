c7454d6393559c140440e2bccdd89b874d537431
anasds790-spec <aapka_email@example.com>
2026-07-23 00:39:20 +0500

    Add new features and fix minor bugs

--- Practice.py/set Favourite_Fruit.py
+++ Practice.py/set Favourite_Fruit.py
@@ -1,5 +1,5 @@
 print("\tDisplay a Favourite Fruits in a Set.")
 Favourite_Fruit={"Apple","Mango","Banana","Grapes"}
-Favourite_Fruit.add("Pineapple")
+Favourite_Fruit.add("PineApple")
 Favourite_Fruit.remove("Banana")
 print("Your Favourite Set is:",Favourite_Fruit)
